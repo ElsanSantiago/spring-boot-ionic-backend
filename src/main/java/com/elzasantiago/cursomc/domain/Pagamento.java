2da712bcaf310f708d7b16cafd5f363fa298e6b0
Elza Santiago <elzansantiago@gmail.com>
2019-01-02 18:52:44 -0200

    Atualização usando soment o JsonIgnore

--- src/main/java/com/elzasantiago/cursomc/domain/Pagamento.java
+++ src/main/java/com/elzasantiago/cursomc/domain/Pagamento.java
@@ -11,7 +11,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.elzasantiago.cursomc.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -22,7 +22,7 @@ private static final long serialVersionUID = 1L;
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
